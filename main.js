99d3d33716820b434c125b3c3c228d878c85c0b4
kunyaoxu <29974104+kunyaoxu@users.noreply.github.com>
2021-02-04 02:44:33 +0800

    use submodule

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log('hello world');
+const Constant = require("./sub-module/constant/constant");
+
+const { HELLO, WORLD } = Constant;
+
+console.log("hi");
+console.log(`${HELLO} ${WORLD}`);
